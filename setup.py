65c4fe0d4022ced471d5795b80721285b7e1d34f
Niklas Henning <niklashenningbusiness@gmail.com>
2024-06-23 22:20:10 +0200

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='pyqt-advanced-slider',
-    version='1.1.0',
+    version='1.1.1',
     author='Niklas Henning',
     author_email='business@niklashenning.com',
     license='MIT',
